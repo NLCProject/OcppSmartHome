49e4b1593bf04e17ce32d02052fe45ac307b298b
mgraf <mgraf3@gmx.at>
2022-09-15 13:44:48 +0200

    Data Handling Service Added

---
+++ src/main/kotlin/org/ocpp/smart/home/services/dataHandling/DataHandlingService.kt
@@ -0,0 +1,36 @@
+package org.ocpp.smart.home.services.dataHandling
+
+import org.battery.controller.util.controller.modbusSimulator.commands.CommandUUID
+import org.battery.controller.util.controller.modbusSimulator.commands.ModbusRequest
+import org.isc.utils.serialization.JsonSerialization
+import org.ocpp.client.event.client.request.ClientDataTransferRequestEvent
+import org.ocpp.smart.home.services.dataHandling.interfaces.IDataHandlingService
+import org.ocpp.smart.home.services.modbus.interfaces.IBatteryControllerService
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class DataHandlingService @Autowired constructor(
+    private val batterController: IBatteryControllerService
+): IDataHandlingService, JsonSerialization() {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    override fun handleIncomingDataTransfer(event: ClientDataTransferRequestEvent) {
+        logger.info("Handling incoming data transfer")
+        parsingAndHandleData(data = event.request.data)
+    }
+
+    private fun parsingAndHandleData(data: String) {
+        logger.info("Parsing MODBUS data | $data")
+        when {
+            data.contains(CommandUUID.modbusRequest) -> {
+                val request = super.decode<ModbusRequest>(data = data)
+                batterController.sendCommand(request = request)
+            }
+
+            else -> throw Exception("Unknown or not defined UUID in received data | $data")
+        }
+    }
+}
---
+++ src/main/kotlin/org/ocpp/smart/home/services/dataHandling/interfaces/IDataHandlingService.kt
@@ -0,0 +1,18 @@
+package org.ocpp.smart.home.services.dataHandling.interfaces
+
+import org.ocpp.client.event.client.request.ClientDataTransferRequestEvent
+import org.springframework.context.event.EventListener
+
+/**
+ *
+ */
+interface IDataHandlingService {
+
+    /**
+     * Incoming data.
+     *
+     * @param event .
+     */
+    @EventListener(ClientDataTransferRequestEvent::class)
+    fun handleIncomingDataTransfer(event: ClientDataTransferRequestEvent)
+}
