e6374a90b48720ed07b3261ffc53767fa36581ae
mgraf <mgraf3@gmx.at>
2022-10-31 17:21:44 +0100

    Sending response on command

--- src/main/kotlin/org/ocpp/smart/home/services/dataHandling/DataHandlingService.kt
+++ src/main/kotlin/org/ocpp/smart/home/services/dataHandling/DataHandlingService.kt
@@ -3,7 +3,9 @@ package org.ocpp.smart.home.services.dataHandling
 import org.battery.controller.util.controller.modbusSimulator.commands.CommandUUID
 import org.battery.controller.util.controller.modbusSimulator.commands.ModbusRequest
 import org.isc.utils.serialization.JsonSerialization
+import org.ocpp.client.client.interfaces.IClientRequestService
 import org.ocpp.client.event.client.request.ClientDataTransferRequestEvent
+import org.ocpp.smart.home.configuration.SmartHomeConfiguration
 import org.ocpp.smart.home.services.dataHandling.interfaces.IDataHandlingService
 import org.ocpp.smart.home.services.modbus.interfaces.IBatteryControllerService
 import org.slf4j.LoggerFactory
@@ -12,7 +14,8 @@ import org.springframework.stereotype.Service
 
 @Service
 class DataHandlingService @Autowired constructor(
-    private val batteryController: IBatteryControllerService
+    private val batteryController: IBatteryControllerService,
+    private val clientRequestService: IClientRequestService
 ): IDataHandlingService, JsonSerialization() {
 
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -28,6 +31,10 @@ class DataHandlingService @Autowired constructor(
             data.contains(CommandUUID.modbusRequest) -> {
                 val request = super.decode<ModbusRequest>(data = data)
                 val response = batteryController.sendCommand(request = request)
+                clientRequestService.dataTransfer(
+                    vendorId = SmartHomeConfiguration.vendorId,
+                    data = super.encode(model = response)
+                )
             }
 
             else -> throw Exception("Unknown or not defined UUID in received data | $data")
